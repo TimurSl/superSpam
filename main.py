3a59ba70224364a9e713040e978c1db55df4f843
BuildTools <unconfigured@null.spigotmc.org>
2022-09-06 12:59:44 +0300

    Clean code

--- main.py
+++ main.py
@@ -11,34 +11,17 @@ import yaml
 msg = "Spam"
 interval = 0
 spam_button = "f5"
-# is_running: bool = False
-
-# def start_spam():
-#     while True:
-#         if is_running:
-#             send_msg(msg)
 
 def send_msg(msg):
     key.write(msg)
     key.press_and_release("enter")
     time.sleep(interval)
 
-# def switch_is_run():
-#     global is_running
-#
-#     if is_running == True:
-#         is_running = False
-#         print(is_running)
-#     else:
-#         is_running = True
-#         start_spam()
-
 def main():
     global msg
     global interval
     global spam_button
 
-    # keyboard.add_hotkey("alt+s", exit())
     if Path("config.yaml").is_file():
         with codecs.open('config.yaml') as f:
             config = yaml.safe_load(f)
@@ -46,12 +29,7 @@ def main():
             msg = config["spam-msg"]
             interval = config["interval"]
             spam_button = config["spam-button"]
-            # if config["enable-switcher"] == False:
             key.add_hotkey(spam_button, lambda: send_msg(msg))
-            #     print("Switcher disabled!")
-            # else:
-            #     key.add_hotkey(spam_button, lambda: switch_is_run())
-            #     print("Switched enabled!")
     else:
         print("Not found config file, creating...")
         f = open("config.yaml", "w+")
@@ -59,7 +37,6 @@ def main():
             "interval: 0\n"
             "spam-msg: 'Youre bad-ass!'\n"
             "spam-button: f5\n"
-            # "enable-switcher: False"
         )
         f.close()
         print("Program closes in 5 seconds")
@@ -74,17 +51,15 @@ def main():
         "Interval: " + str(interval) + "\n"
         "Message: " + msg + "\n"
         "Button: " + spam_button + "\n"
-        # "Enable switcher: " + str(config["enable-switcher"]) + "\n"
 
     )
 
     while True:
-        if key.read_key() == "o":
+        # dont close program
+        if key.read_key() == "alt+s":
             print("App closed")
             exit()
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
